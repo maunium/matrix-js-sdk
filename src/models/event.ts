6446829c5d26b4c42fa854c9213f84c83490af60
Tulir Asokan <tulir@maunium.net>
2022-11-05 23:21:06 +0200

    Allow room moderators to view redacted event content
    Implements MSC2815
    
    Signed-off-by: Tulir Asokan <tulir@maunium.net>

--- src/models/event.ts
+++ src/models/event.ts
@@ -406,6 +406,11 @@ export class MatrixEvent extends TypedEventEmitter<MatrixEventEmittedEvents, Mat
      */
     public forwardLooking = true;
 
+    /* flag to indicate that the event was redacted, but we fetched the
+     * unredacted content from the server using room moderator powers.
+     */
+    public viewingRedactedContent = false;
+
     private readonly reEmitter: TypedReEmitter<MatrixEventEmittedEvents, MatrixEventHandlerMap>;
 
     /**
@@ -1313,7 +1318,7 @@ export class MatrixEvent extends TypedEventEmitter<MatrixEventEmittedEvents, Mat
      * @returns True if this event has been redacted
      */
     public isRedacted(): boolean {
-        return Boolean(this.getUnsigned().redacted_because);
+        return Boolean(this.getUnsigned().redacted_because && !this.viewingRedactedContent);
     }
 
     /**
@@ -1448,6 +1453,19 @@ export class MatrixEvent extends TypedEventEmitter<MatrixEventEmittedEvents, Mat
             this.emit(MatrixEventEvent.LocalEventIdReplaced, this);
         }
 
+        this.localTimestamp = Date.now() - (this.getAge() ?? 0);
+    }
+
+    public showRedactedContent(event: object): void {
+        const oldUnsigned = this.getUnsigned();
+        this.event = event;
+        this.clearEvent = undefined;
+        if (!this.event.unsigned) {
+            this.event.unsigned = {};
+        }
+        this.event.unsigned.redacted_because = oldUnsigned.redacted_because;
+        this.viewingRedactedContent = true;
+        this.setStatus(null);
         this.localTimestamp = Date.now() - (this.getAge() ?? 0);
     }
 
